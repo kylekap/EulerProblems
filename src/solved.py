f6f96c9aec73b9e51a58a5db627189b66d28026c
Kyle Patterson <kylekaptbone@yahoo.com>
2025-11-19 10:39:58 -0800

    Solved 72

--- src/solved.py
+++ src/solved.py
@@ -1904,3 +1904,18 @@ def problem71(max_d=1000000, target_fraction=(3, 7)): #TODO(Kyle): #12 Should be
                 left_fraction = (n/d, util.reduce_fraction(n, d)) # Use it
             continue
     return left_fraction[1]
+
+
+def problem72(max_d=1000000):
+    """Euler Problem 72: Counting fractions in a range.
+
+    Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+    If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+    1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+    It can be seen that there are 21 elements in this set.
+    How many elements would be contained in the set of all reduced proper fractions for d ≤ 1,000,000?
+    """
+    x = 0
+    for d in range(2,max_d+1): #Go through all denominators
+        x+=util.eulers_totient(d) #Number of reduced proper fractions for denominator d is φ(d)
+    return x
