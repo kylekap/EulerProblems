034055c004c83d8497dd6a1b1140ab81baff5a37
Kyle Patterson <kylekaptbone@yahoo.com>
2025-12-19 16:37:32 -0800

    Problem 91 progress

--- src/core.py
+++ src/core.py
@@ -10,7 +10,31 @@ logger = logging.getLogger(__name__)  # put this in each file
 
 
 def active_problem():
-    return None
+    # Terrible answer to #91. Way too slow - need to find a better way
+    def find_chain(x, max_limit=1_000_000):
+        loop_val = x
+        chain = [x]
+        while True:
+            fd = util.add_list(list(set(util.find_all_divisors(loop_val))))
+            if fd == chain[0]:
+                break
+            if fd in chain:
+                chain = []
+                break
+            if max_limit and fd > max_limit:
+                chain = []
+                break
+            chain.append(fd)
+            loop_val = fd
+        return chain
+    max_len = 0
+    max_chain = []
+    for x in range(1, 1_000_000):
+        chain = find_chain(x)
+        if len(chain) > max_len:
+            max_len = len(chain)
+            max_chain = chain
+    return min(max_chain), max_chain
 
 
 def main():
